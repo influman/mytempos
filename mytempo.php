6517f64440a4a1f55f4d42f76b02add23d4dea1d
influman <sylvain.martinezgil@gmail.com>
2018-02-04 10:53:20 +0100

    1st

---
+++ mytempo.php
@@ -0,0 +1,55 @@
+<?php
+   //***********************************************************************************************************************
+   // V1.0 : Script qui permet la gestion de temporisations, avec extinction automatique de lampe en option
+   //*************************************** API eedomus ******************************************************************
+   
+   // recuperation des infos depuis la requete
+  
+  // n° de tempo : 1 à 20
+  $tempo=getArg("tempo", $mandatory = false, $default = 1);
+  // si API lampe associée pour extinction
+  $periphId=getArg("periphId", $mandatory = false, $default = '');
+  // action
+  $action=getArg("action", $mandatory = false, $default = '');
+  // delai
+  $delai=getArg("delai", $mandatory = false, $default = 0);
+
+   
+// Lecture des tempos en variable générale de l'eedomus et décrémentation
+If ($action == 'xml') {
+   $xml="<TEMPOS>";
+   if (loadVariable('mytempo'.$tempo)) {
+	 $tempoValue=loadVariable('mytempo'.$tempo);
+	// Décrémentation automatique du délai
+	if ($tempoValue <> 0) {
+	  $tempoValue = $tempoValue - 1;
+	  // Extinction de la lampe si API donnée et tempo à 0
+			if ($periphId && $periphId != 'undefined' && $tempoValue == 0) {
+      			SetValue($periphId, 0);
+   			} 
+      }
+   } else {
+	  // mise à zéro générale la première fois
+		$tempoValue = 0;
+   }
+   saveVariable('mytempo'.$tempo,$tempoValue);
+   $xml .= "<TEMPO".$tempo.">".$tempoValue."</TEMPO".$tempo.">";
+   $xml .= "</TEMPOS>";
+   sdk_header('text/xml');
+   echo $xml;
+}
+   
+// Réinitialisation demandée du tempo désigné
+   if ($action == 'init' && $delai <> 0) {
+	$delai = $delai + 1;
+	saveVariable('mytempo'.$tempo,$delai);
+	die();
+   }
+  
+// Annulation demandée du tempo désigné
+   if ($action == 'stop') {
+	$delai = 0;
+	saveVariable('mytempo'.$tempo,$delai);
+	die();
+   }
+?>
